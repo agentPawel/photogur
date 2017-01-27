ed586d565291103156a904b954bf35407d2b5f37
Pawel Parafianowicz <pawelp@outlook.com>
2017-01-27 15:18:14 -0500

    Fix minor error in controller for creating photo

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,10 +7,6 @@ class PicturesController < ApplicationController
     @picture = Picture.find(params[:id])
   end
 
-  def create
-    render text: "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
-  end
-
   def new
     @pictuure = Picture.new
   end
